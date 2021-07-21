d6f297a6eb09816097f3c42c5f3abce8921317d0
Shrish Sharma <shrish0608@gmail.com>
2021-07-21 18:06:42 +0530

    Added Material Icons Package

--- src/components/MakeNote.js
+++ src/components/MakeNote.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button, Container, Typography } from '@material-ui/core';
 
 const MakeNote = () => {
     return (  
-        <div className="make-note">
-            Make Note Here.
-        </div>
+        <Container>
+            <Typography
+                variant="h6"
+                component="h2"
+                color="primary"
+                gutterBottom
+            >
+                Create a Sticky Note
+            </Typography>
+            <Button
+                onClick={() => console.log('Clicked!')}
+                type="submit"
+                color="secondary"
+                variant="contained">
+                Make Note        
+            </Button>        
+        </Container>
     );
 }
  
