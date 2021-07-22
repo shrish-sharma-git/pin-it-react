2fa41d5ec163504e84989d1c36f3209c25d25481
Shrish Sharma <shrish0608@gmail.com>
2021-07-22 17:07:31 +0530

    Create Note Form Validation

--- src/components/MakeNote.js
+++ src/components/MakeNote.js
@@ -1,24 +1,86 @@
 import React from 'react';
-import { Button, Container, Typography } from '@material-ui/core';
+import { Button, Container, Typography, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { useState } from 'react';
+
+// Custom CSS Hook
+const useStyles = makeStyles({
+    field: {
+        marginTop: 20,
+        marginBottom: 20,
+        display: 'block'
+    }
+})
 
 const MakeNote = () => {
+    // Custom CSS Hook Ref
+    const classes = useStyles();
+
+    // State Hooks for TextFields
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const [titleError, setTitleError] = useState(false);
+    const [contentError, setContentError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setTitleError(false);
+        setContentError(false);
+
+        if(title === ''){
+            setTitleError(true);
+        }
+
+        if(content === ''){
+            setContentError(true);
+        }
+
+        if(title && content){
+            console.log(title, content);
+        }
+    }
+
     return (  
         <Container>
             <Typography
                 variant="h6"
                 component="h2"
                 color="primary"
+                align="center"
                 gutterBottom
             >
                 Create a Sticky Note
             </Typography>
-            <Button
-                onClick={() => console.log('Clicked!')}
-                type="submit"
-                color="secondary"
-                variant="contained">
-                Make Note        
-            </Button>        
+            
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
+                    className={classes.field}
+                    onChange={(e) => setTitle(e.target.value)}
+                    label="Title"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    error={titleError}
+                />
+                <TextField 
+                    className={classes.field}
+                    onChange={(e) => setContent(e.target.value)}
+                    label="Content"
+                    variant="outlined"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    required
+                    error={contentError}
+                />
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained">
+                    Make Note        
+                </Button>     
+            </form>   
         </Container>
     );
 }
