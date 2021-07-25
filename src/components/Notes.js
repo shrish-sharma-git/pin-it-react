0cc731b6c037d1079ed517a9856b97ee64ac1f1f
Shrish Sharma <shrish0608@gmail.com>
2021-07-25 12:48:26 +0530

    Note Filter wrt User Logged In

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,15 +4,18 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { firestore } from '../firebase/config';
 import NoteCard from './parts/StickyCard';
+import {useAuth} from '../context/authContext';
 
 const Notes = () => {
     // State Hook
     const [notes, setNotes] = useState([]);
+    const { currentUser } = useAuth();
+    const userUid = currentUser.uid;
 
     // Fetching Data
     useEffect(() => {
         const data = firestore.collection('notes')
-            .orderBy('createdAt', 'desc')
+            .where('authorId', '==', userUid)     
             .get()
             .then(snap => {
                 const documents = [];
